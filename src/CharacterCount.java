661dfcc8fdf33d086ddb794146a54be2522dfa56
Ren103 <fransili2702@gmail.com>
2023-03-09 17:43:55 +0300

    Push main files

---
+++ src/CharacterCount.java
@@ -0,0 +1,77 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CharacterCount {
+
+    /**
+     * counts the number of each letter in the file, including uppercase and lowercase letters
+     */
+    public static void count() {
+        // Ask the user for the name of the input file
+        BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
+        System.out.println("Enter the name of the input file:");
+        String fileName = null;
+        try {
+            fileName = reader.readLine();
+        } catch (IOException e) {
+            System.out.println("Error reading input: " + e.getMessage());
+            System.exit(1);
+        }
+
+        // Check if the input file exists
+        java.io.File inputFile = new java.io.File(fileName);
+        if (!inputFile.exists()) {
+            System.out.println("Error: input file does not exist");
+            System.exit(1);
+        }
+
+        // Initialize the letter count arrays
+        int[] upperCaseCount = new int[26];
+        int[] lowerCaseCount = new int[26];
+
+        // Read the input file and count the letters
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(inputFile))) {
+            String line;
+            while ((line = fileReader.readLine()) != null) {
+                for (int i = 0; i < line.length(); i++) {
+                    char c = line.charAt(i);
+                    if (Character.isUpperCase(c)) {
+                        upperCaseCount[c - 'A']++;
+                    } else if (Character.isLowerCase(c)) {
+                        lowerCaseCount[c - 'a']++;
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading input file: " + e.getMessage());
+            System.exit(1);
+        }
+
+        // Ask the user for the name of the output file
+        System.out.println("Enter the name of the output file:");
+        String outputFileName = null;
+        try {
+            outputFileName = reader.readLine();
+        } catch (IOException e) {
+            System.out.println("Error reading input: " + e.getMessage());
+            System.exit(1);
+        }
+
+        // Write the letter count results to the output file
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName))) {
+            for (int i = 0; i < 26; i++) {
+                writer.write((char) ('A' + i) + ": " + upperCaseCount[i] + "\n");
+                writer.write((char) ('a' + i) + ": " + lowerCaseCount[i] + "\n");
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing output file: " + e.getMessage());
+            System.exit(1);
+        }
+
+        // Print a success message
+        System.out.println("Letter count successfully written to " + outputFileName);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        CharacterCount cc = new CharacterCount();
+        cc.count();
+    }
+}
