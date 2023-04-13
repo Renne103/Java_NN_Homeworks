a8489a1c718c527a168f688e87192f466c1d5f77
RennI <98748434+Renne103@users.noreply.github.com>
2023-04-13 16:00:00 +0300

    Update PersonGUI.java

--- Laba3/src/PersonGUI.java
+++ Laba3/src/PersonGUI.java
@@ -12,11 +12,11 @@ import java.time.format.DateTimeFormatter;
  person's full name, gender, and age in years.
  */
 public class PersonGUI {
-    private JTextField firstNameField;
+    private JTextField NameField;
     private JTextField lastNameField;
     private JTextField middleNameField;
-    private JTextField birthDateField;
-    private JButton submitButton;
+    private JTextField birthField;
+    private JButton button;
     private JTextArea resultArea;
     /**
      * Constructs a new PersonGUI object and sets up the graphical user interface.
@@ -27,55 +27,49 @@ public class PersonGUI {
         frame.setSize(500, 300);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Создаем панель с элементами формы
+
         JPanel formPanel = new JPanel(new GridLayout(5, 2));
         formPanel.add(new JLabel("Фамилия:"));
         lastNameField = new JTextField();
         formPanel.add(lastNameField);
         formPanel.add(new JLabel("Имя:"));
-        firstNameField = new JTextField();
-        formPanel.add(firstNameField);
+        NameField = new JTextField();
+        formPanel.add(NameField);
         formPanel.add(new JLabel("Отчество:"));
         middleNameField = new JTextField();
         formPanel.add(middleNameField);
         formPanel.add(new JLabel("Дата рождения:"));
-        birthDateField = new JTextField("dd.mm.yyyy");
-        formPanel.add(birthDateField);
-        submitButton = new JButton("Получить результат");
-        formPanel.add(submitButton);
+        birthField = new JTextField("dd.mm.yyyy");
+        formPanel.add(birthField);
+        button = new JButton("Получить результат");
+        formPanel.add(button);
 
-        // Создаем панель для вывода результата
         resultArea = new JTextArea();
         resultArea.setEditable(false);
         JScrollPane scrollPane = new JScrollPane(resultArea);
 
-        // Добавляем обработчик клика по кнопке "Получить результат"
-        submitButton.addActionListener(new ActionListener() {
+        button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String firstName = firstNameField.getText();
+                String firstName = NameField.getText();
                 String lastName = lastNameField.getText();
                 String middleName = middleNameField.getText();
-                String birthDateString = birthDateField.getText();
+                String birthDateString = birthField.getText();
 
-                // Создаем объект класса Person с введенными данными
                 Person person = new Person(firstName, lastName, middleName, birthDateString);
 
-                // Формируем текст для вывода результата
                 String result = String.format("ФИО: %s %s.%s.\nПол: %s\nВозраст: %s",
                         person.getLastName(), person.getFirstName().charAt(0), person.getMiddleName().charAt(0),
                         person.getGender(), person.getAgeInYears());
 
-                // Выводим результат в текстовую область
+
                 resultArea.setText(result);
             }
         });
 
-        // Добавляем панели на главное окно
         frame.add(formPanel, BorderLayout.NORTH);
         frame.add(scrollPane, BorderLayout.CENTER);
 
-        // Показываем окно
         frame.setVisible(true);
     }
 }
